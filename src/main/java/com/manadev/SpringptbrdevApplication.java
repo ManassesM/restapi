59b0f883ee8445b2122b65e9f636f77d49db9c9f
ManassesM <manmiui2001@gmail.com>
2020-08-26 20:00:04 -0300

    Salvando arquivo no S3

--- src/main/java/com/manadev/SpringptbrdevApplication.java
+++ src/main/java/com/manadev/SpringptbrdevApplication.java
@@ -1,13 +1,26 @@
 package com.manadev;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.manadev.services.S3Service;
+
 @SpringBootApplication
-public class SpringptbrdevApplication {
+public class SpringptbrdevApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringptbrdevApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\fotos\\office1.jpg");
+
 	}
 
 }
